d127c524f141b41f631703897edc720e3118fa6e
Nirnaya Betal <83304816+NNBSTRIKERS@users.noreply.github.com>
2023-06-28 20:17:10 +0530

    Time: 2802 ms (10.11%) | Memory: 0B (100.00%) - LeetSync

--- department-top-three-salaries/department-top-three-salaries.sql
+++ department-top-three-salaries/department-top-three-salaries.sql
@@ -1,11 +1,7 @@
-/* Write your PL/SQL query statement below */
-select d.Name as "Department", 
-    e1.Name as "Employee", 
-    e1.Salary
-from Employee e1
-inner join Department d 
-on e1.DepartmentId = d.Id
-where 3 > (select count(distinct e2.Salary)
-            from Employee e2
-            where e2.Salary > e1.Salary
-                and e1.DepartmentId = e2.DepartmentId);
+# Write your MySQL query statement below
+select d.name as department , e1.name as employee, e1.salary as Salary
+from Employee e1 join Department d on e1.DepartmentId = d.Id
+where  3 > (select count(distinct (e2.Salary))
+        from  Employee e2
+        where e2.Salary > e1.Salary
+            and e1.DepartmentId = e2.DepartmentId)
